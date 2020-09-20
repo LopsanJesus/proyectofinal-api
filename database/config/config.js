9c3a6c05620cc65615b5f870951cce9b7fe1ef9b
Jesús López Sánchez <lopsanjesus@gmail.com>
2020-09-20 18:39:53 +0200

    Add migrations

--- database/config/config.js
+++ database/config/config.js
@@ -1,12 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
-    dialect: 'postgres',
-  },
-  test: {
-    url: process.env.TEST_DATABASE_URL,
     dialect: 'postgres',
   },
   production: {
