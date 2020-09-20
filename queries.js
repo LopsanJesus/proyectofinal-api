40ae3f01974b0f1cfbe915da222a0bfee0dde09a
Jesús López Sánchez <lopsanjesus@gmail.com>
2020-09-20 13:53:31 +0200

    Add sequelize and new database config

--- queries.js
+++ queries.js
@@ -1,8 +1,8 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'ltarrbliehoikx', 
-    host: 'ec2-46-137-156-205.eu-west-1.compute.amazonaws.com', 
-    database: 'ddi3lnaobbpnta', 
-    password: '0afe44e24e605694f3efc7c30ba8e5f5669821090736d226a4f56eabaf0ac279',
+    user: 'ysdfrklyjigiph', 
+    host: 'ec2-54-217-236-206.eu-west-1.compute.amazonaws.com', 
+    database: 'd5thoeukc04mjn', 
+    password: '5daddfc91d3e8950ce1b90435ef188e2e0b0ef690561f8ea55eeae7787a2874c',
     port: 5432 
 });
