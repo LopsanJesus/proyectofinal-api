2529f7d4c335be9ec412cfef4c3035f956c26c9a
Jesús López Sánchez <lopsanjesus@gmail.com>
2020-10-20 01:58:55 +0200

    New models/index.js

--- database/models/index2.js
+++ database/models/index2.js
@@ -1,21 +1,18 @@
 
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const envConfigs =  require('../config/config');
-
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = envConfigs[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.url) {
-  //console.log(config);
-  //console.log(config.url);
-  sequelize = new Sequelize(config.url, config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  //console.log(config);
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
@@ -25,20 +22,18 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    //console.log(file);
-    //const model = sequelize['import'](path.join(__dirname, file));
-    var model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(modelName => {
-  console.log(modelName);
-  if (db[modelName].associate) {    
-    db[modelName].associate(db);  
-    }
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
