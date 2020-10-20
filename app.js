7838de11a71e1399a15d97bab80023d0e83223a1
Jesús López Sánchez <lopsanjesus@gmail.com>
2020-10-20 02:08:31 +0200

    Correct Sequelize issues

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ const resolvers = require('./resolvers')
 const models = require('./database/models');
 const server = new ApolloServer({ typeDefs, resolvers, context: { models } });
 const app = express();
+
+var indexRouter = require('./routes/index');
+
+//var cors = require('cors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+
 server.applyMiddleware({ app });
 models.sequelize.authenticate();
 models.sequelize.sync();
