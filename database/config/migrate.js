bbb8b12dad1d01e77530f5f2e429660ea4658bad
LopsanJesus <55506008+LopsanJesus@users.noreply.github.com>
2020-11-01 16:27:32 +0100

    Update database config

--- database/config/migrate.js
+++ database/config/migrate.js
@@ -1,4 +1,4 @@
-const models = require('./database/models');
+const models = require('../models');
 
 models.sequelize.authenticate();
 models.sequelize.sync();
--- database/migrations/20201025195613-create-language.js
+++ database/migrations/20201025195613-create-language.js
@@ -2,12 +2,18 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Languages', {
-      code: {
+      id: {
         allowNull: false,
+        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      code: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
