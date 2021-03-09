e3dcfefda933cdd9cfd009af8da68523f6a124f4
Jesús López Sánchez <lopsanjesus@gmail.com>
2021-03-09 23:41:37 +0100

    Fix ssl problem

--- database/models/index.js
+++ database/models/index.js
@@ -1,35 +1,40 @@
-require('dotenv').config()
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const db = {};
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_DB, 
-  process.env.DATABASE_USERNAME, 
-  process.env.DATABASE_PASSWORD, 
+  process.env.DATABASE_DB,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
   {
     dialect: process.env.DATABASE_DIALECT,
     host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT
+    port: process.env.DATABASE_PORT,
+    ssl: true,
   }
 );
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
-    var model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    var model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {    
-    db[modelName].associate(db);  
-    }
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
